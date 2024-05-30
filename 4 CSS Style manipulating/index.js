fe0b514acc1d4e54cef352b4dffe73e17674a4b2
Tushar_Hossain <tusharhossain455@gmail.com>
2024-05-31 01:56:25 +0600

    css style manipulating

---
+++ 4 CSS Style manipulating/index.js
@@ -0,0 +1,10 @@
+// CSS Style manipulating
+
+// $("h1").css("font-size", "3rem");
+// $("h1").css("color", "tomato");
+
+// json formate css style
+// $("h1").css({ "font-size": "3rem", color: "tomato", "font-style": "italic" });
+
+// css class list adding by jquery
+$("h1").addClass("style style2 style3");
