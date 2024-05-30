8150285a9a0adc8e480c6d534cfb677bd8080ef5
Tushar_Hossain <tusharhossain455@gmail.com>
2024-05-30 19:00:19 +0600

    introduction to jquery

---
+++ 1 Introduction to jQuery/index.js
@@ -0,0 +1,9 @@
+// Introduction to jQuery
+
+// document.querySelector("h1").innerHTML = "goodbye";
+
+$("h1").text("goodbye");
+
+$("p").text(
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A placeat non at consequuntur, vel corporis aliquam, corrupti quaerat ea molestias quidem quo nihil reiciendis culpa provident! Eos magni commodi earum."
+);
