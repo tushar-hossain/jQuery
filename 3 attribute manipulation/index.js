f250ae97dcf88e43e5b04716c9f1fc34152eb24f
Tushar_Hossain <tusharhossain455@gmail.com>
2024-05-31 02:00:56 +0600

    attribute manipulation

---
+++ 3 attribute manipulation/index.js
@@ -0,0 +1,18 @@
+// attribute manipulation
+
+// get attribute
+// let attr = document.querySelector("a").getAttribute("href");
+// console.log(attr);
+
+// let attr2 = $("a").attr("href");
+// console.log(attr2);
+
+// remove attribute
+// let attrm = $("a").removeAttr("href");
+
+// console.log(attrm);
+
+// set attribute
+// document.querySelector("a").setAttribute("href", "https://facebook.com");
+
+$("a").attr("href", "https://facebook.com");
